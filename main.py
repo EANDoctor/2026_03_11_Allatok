10bde2b164bdac24754b942c9696874c18bfb584
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2026-03-11 07:46:54 +0100

    Allat; osztaly létrehozása

---
+++ main.py
@@ -0,0 +1,10 @@
+from allat import Allat
+import random as r
+import os
+
+
+allat = Allat("Bodri", "kutya", 5, "kert", "közepes")
+allat2 = Allat("Cirmi", "macska", 2, "ház", "közepes")
+
+print(allat)
+print(allat2)
