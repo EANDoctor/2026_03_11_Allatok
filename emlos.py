ec0ca6a57d4757d51a65c30c9d7918261123a677
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2026-03-11 08:46:39 +0100

    kutya, cica

--- emlos.py
+++ emlos.py
@@ -1,10 +1,24 @@
 from allat import Allat
 
 class Emlos(Allat):
-    def __init__(self, nev_, faj_, eletkor_, elohely_, meret_, szorzet_szine):
+    def __init__(self, nev_, faj_, eletkor_, elohely_, szorzet_szine):
         super().__init__(nev_, faj_, eletkor_, elohely_, "közepes")
         self.szorzet_szine = szorzet_szine
 
     def __str__(self):
-        return super().__init__() + f", szőrzete: {self.szorzet_szine}"
+        return super().__str__() + f", szőrzete: {self.szorzet_szine}"
         # return f"{self.nev} ({self.faj}), {self.eletkor} éves, élőhelye: {self.elohely}, mérete: {self.meret}, szőrzete: {self.szorzet_szine}"
+
+class Macska(Emlos):
+    def __init__(self, nev_, eletkor_, elohely_, szorzet_szine):
+        super().__init__(nev_, "macska", eletkor_, elohely_, szorzet_szine)
+
+    def dorombol(self):
+        print(f"{self.nev} épp dorombol.")
+
+class Kutya(Emlos):
+    def __init__(self, nev_, eletkor_, elohely_, szorzet_szine):
+        super().__init__(nev_, "kutya", eletkor_, elohely_, szorzet_szine)
+
+    def ugat(self):
+        print(f"{self.nev} épp ugat.")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from allat import Allat
-from emlos import Emlos
+from emlos import Emlos, Macska, Kutya
 
 allat = Allat("Bodri", "kutya", 5, "kert", "közepes")
 allat2 = Allat("Cirmi", "macska", 2, "ház", "közepes")
@@ -13,3 +13,11 @@ emlos = Emlos("Morzsi", "kutya", 5, "kert", "barna")
 emlos2 = Emlos("Cirma", "macska", 3, "lakás", "fekete")
 
 print(emlos)
+
+
+
+macska1 = Macska("Hubert", 4, "ház", "fehér")
+print(macska1)
+
+kutya1 = Kutya("Rex", 6, "kert", "fekete")
+print(kutya1)
